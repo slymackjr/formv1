553797d55dfb478fadf3515e2aa180f9f38fdd2a
Jofrey Nyamasheki <jbnyamasheki@gmail.com>
2022-12-16 12:38:05 +0300

    PHP web pages

---
+++ php/form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Farmer Registered</title>
+</head>
+<body>
+	<?php
+	//if(isset($_POST['submit']))
+	if (!empty($_POST['submit'])) {
+		// code...
+		$firstName=$_POST['firstname'];
+		$middleName=$_POST['middlename'];
+		$surName=$_POST['surname'];
+	}
+	?>
+	<div>
+		<div>
+			<div>
+				<h1>WELCOME TO OUR ALL MULTI PLATFORM</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
